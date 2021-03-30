cb2a6ce31d954f7789ddcdabb2fab2cb87fdb2a3
Gustavo Nogueira <gsn181200@gmail.com>
2021-03-29 21:32:24 -0300

    Port Altered

--- Frontend/src/service/api.ts
+++ Frontend/src/service/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: 'http://localhost:3333/'
-    baseURL: "http://3004cdbf9b26.ngrok.io"
+    //baseURL: "http://3004cdbf9b26.ngrok.io"
+    baseURL: "https://mobilelolpocketprofile.herokuapp.com/"
 })
 
 export default api;
